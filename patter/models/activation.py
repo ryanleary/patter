b1a025080495f6b5c64fcb6fa8b5b4b5943a2c6d
Ryan Leary <ryanleary@gmail.com>
2018-01-31 18:12:43 -0500

    Add new activations

---
+++ patter/models/activation.py
@@ -0,0 +1,36 @@
+import torch
+from torch.nn import Module, functional as F
+
+
+class Swish(Module):
+    """Implementation of Swish: a Self-Gated Activation Function
+        Swish activation is simply f(x)=x⋅sigmoid(x)
+        Paper: https://arxiv.org/abs/1710.05941
+    Shape:
+        - Input: :math:`(N, *)` where `*` means, any number of additional
+          dimensions
+        - Output: :math:`(N, *)`, same shape as the input
+    Examples::
+        >>> m = nn.Swish()
+        >>> x = autograd.Variable(torch.randn(2))
+        >>> print(x)
+        >>> print(m(x))
+    """
+
+    def forward(self, input_):
+        return input * torch.sigmoid(input_)
+
+    def __repr__(self):
+        return self.__class__.__name__ + '()'
+
+
+class InferenceBatchSoftmax(Module):
+    """ Implementation of the Softmax activation, that is only
+        run during inference. During training, the input is
+        returned unchanged.
+    """
+    def forward(self, input_, dim=-1):
+        if not self.training:
+            return F.softmax(input_, dim=dim)
+        else:
+            return input_
